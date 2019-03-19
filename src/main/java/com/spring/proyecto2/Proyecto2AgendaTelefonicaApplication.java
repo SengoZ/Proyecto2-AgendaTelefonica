a95f45694c821bab756b35e15e1c8c42d6634442
andres_g <gomezs.jandres@gmail.com>
2019-03-19 16:13:36 +0100

    Esqueleto del proyecto

---
+++ src/main/java/com/spring/proyecto2/Proyecto2AgendaTelefonicaApplication.java
@@ -0,0 +1,13 @@
+package com.spring.proyecto2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyecto2AgendaTelefonicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyecto2AgendaTelefonicaApplication.class, args);
+	}
+
+}
