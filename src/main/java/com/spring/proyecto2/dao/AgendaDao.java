15abf058b61aed538b12d1673ca489fbe9d2ffce
andres_g <gomezs.jandres@gmail.com>
2019-03-20 14:36:40 +0100

    Interfaz DAO: Inclusión del metodo buscarId()

--- src/main/java/com/spring/proyecto2/dao/AgendaDao.java
+++ src/main/java/com/spring/proyecto2/dao/AgendaDao.java
@@ -28,5 +28,12 @@ public interface AgendaDao {
 	 */
 	public void add(Persona persona);
 	
-
+	/**
+	 * Método que devuelve el contacto de la agenda a partir de un id como parámetro
+	 * @param id Introducimos el identificador del usuario para poder obtener al contacto 
+	 * 				buscado
+	 * @return persona Nos devolverá una persona en caso de encontrarla, si no lo puede hacer 
+	 * 					nos devolverá un elemento nulo.
+	 */
+	public Persona buscarId(int id);
 }
