cd974339c918d2d1985b832fce4bcde26be9fce2
andres_g <gomezs.jandres@gmail.com>
2019-03-20 14:44:39 +0100

    Pruebas

--- src/main/java/com/spring/proyecto2/dao/AgendaDao.java
+++ src/main/java/com/spring/proyecto2/dao/AgendaDao.java
@@ -31,9 +31,9 @@ public interface AgendaDao {
 	/**
 	 * Método que devuelve el contacto de la agenda a partir de un id como parámetro
 	 * @param id Introducimos el identificador del usuario para poder obtener al contacto 
-	 * 				buscado
+	 * buscado
 	 * @return persona Nos devolverá una persona en caso de encontrarla, si no lo puede hacer 
-	 * 					nos devolverá un elemento nulo.
+	 * nos devolverá un elemento nulo.
 	 */
 	public Persona buscarId(int id);
 }
