f34df5c9ec6bddd41296e5652037ae36bf48aee9
admin <admin@Profesor>
2019-03-20 10:47:19 +0100

    Se crean las clases.Falta JavaDoc de Provincia y telefono

---
+++ src/main/java/com/spring/model/Telefono.java
@@ -0,0 +1,32 @@
+package com.spring.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "relacion_telefonos")
+public class Telefono implements Serializable{
+
+		/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+		@Id
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+		private Integer idTelefono;
+		@Column(name="numero")
+		private int numero;
+		
+		@JoinColumn(name= "persona_id", referencedColumnName = "id")
+		@OneToOne
+		private Persona personaId;
+	
+}
