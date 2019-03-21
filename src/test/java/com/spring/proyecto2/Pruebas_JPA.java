a9a97f9c2a8a5a63bc092c09bea5f442bab2267e
andres_g <gomezs.jandres@gmail.com>
2019-03-21 17:47:55 +0100

    Prueba Unitaria (Andres)

---
+++ src/test/java/com/spring/proyecto2/Pruebas_JPA.java
@@ -0,0 +1,28 @@
+package com.spring.proyecto2;
+
+
+
+import javax.transaction.Transactional;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.spring.proyecto2.dao.AgendaDao;
+
+import junit.framework.TestCase;
+
+
+@RunWith(SpringRunner.class)
+public class Pruebas_JPA extends TestCase{
+	@Autowired
+	AgendaDao repository;
+
+	@Test
+	@Transactional
+	public void testDeleteAutentiaTable() {
+		repository.deleteAll();
+		assertTrue(repository.findAll().size() == 0);
+	}
+}
