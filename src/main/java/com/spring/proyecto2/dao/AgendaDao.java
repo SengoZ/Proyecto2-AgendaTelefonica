7735c65838f41c083578f3c217a9046dd80f4d37
andres_g <gomezs.jandres@gmail.com>
2019-03-21 11:48:45 +0100

    Utilización de JPARepository

--- src/main/java/com/spring/proyecto2/dao/AgendaDao.java
+++ src/main/java/com/spring/proyecto2/dao/AgendaDao.java
@@ -3,7 +3,8 @@
  */
 package com.spring.proyecto2.dao;
 
-import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.spring.proyecto2.model.Persona;
 
@@ -11,42 +12,9 @@ import com.spring.proyecto2.model.Persona;
  * @author Sheila Garcia, Cesar Marcos, Amador Caceres, Andres Gomez
  * @version 1.0
  * 
- * Interfaz de DAO de la aplicación de Agenda telefónica
+ * Interfaz de datos de la aplicación de Agenda telefónica
  *
  */
-public interface AgendaDao {
-	
-	/**
-	 * Método que devuelve el listado de todos los contactos que hay en la agenda
-	 * @return listado de personas incluidas en la agenda
-	 */
-	public List<Persona> list();
-	
-	/**
-	 * Método que nos permite introducir a un nuevo contacto
-	 * @param persona Introducimos al método el contacto que queremos agregar.
-	 */
-	public void add(Persona persona);
-	
-	/**
-	 * Método que devuelve el contacto de la agenda a partir de un id como parámetro
-	 * @param id Introducimos el identificador del usuario para poder obtener al contacto 
-	 * buscado
-	 * @return persona Nos devolverá una persona en caso de encontrarla, si no lo puede hacer 
-	 * nos devolverá un elemento nulo.
-	 */
-	public Persona buscarId(int id);
-	
-	/**
-	 * Método para eliminar un contacto de la agenda a partir de un id como parámetro
-	 * @param id Introducimos el identificador del usuario para buscar el contacto a eliminar
-	 */
-	public void delete(int id);
-	
-	/**
-	 * Método para devolver la ficha de una persona concreta a partir de su identificador
-	 * @param id Pasamos por parametro el identificador concreto.
-	 * @return devolvemos la ficha del contacto deseado.
-	 */
-	public Persona fichaContacto(int id);
+public interface AgendaDao extends JpaRepository<Persona, Long> {
+
 }
--- src/main/java/com/spring/proyecto2/dao/AgendaDaoImpl.java
+++ src/main/java/com/spring/proyecto2/dao/AgendaDaoImpl.java
@@ -1,13 +1,6 @@
 package com.spring.proyecto2.dao;
 
-import java.util.List;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
-
-import com.spring.proyecto2.model.Persona;
 
 /**
  * 
@@ -19,74 +12,13 @@ import com.spring.proyecto2.model.Persona;
  */
 
 @Repository
-public class AgendaDaoImpl implements AgendaDao{
+public class AgendaDaoImpl{
 	
-	/**
-	 * Definición de la unidad de persistencia y constructor vacío
-	 */
-	@PersistenceContext	
-	private EntityManager entityManager;
+	private final AgendaDao repository;
 	
-	public AgendaDaoImpl() {
-		super();
-	}
-
-	/**
-	 * Método que devuelve el listado de todos los contactos que hay en la agenda
-	 * @return listado de personas incluidas en la agenda
-	 */
-	@SuppressWarnings("unchecked")
-	@Override
-	@Transactional
-	public List<Persona> list() {
-		String hql = "FROM Persona ORDER BY ID";
-		return (List<Persona>) entityManager.createQuery(hql).getResultList();
-	}
-
-	/**
-	 * /**
-	 * Método que nos permite introducir a un nuevo contacto
-	 * @param persona Introducimos al método el contacto que queremos agregar.
-	 */
-	@Override
-	public void add(Persona persona) {
-		entityManager.merge(persona);
-		
-	}
-
-	/**
-	 * /**
-	 * Método que devuelve el contacto de la agenda a partir de un id como parámetro
-	 * @param id Introducimos el identificador del usuario para poder obtener al contacto 
-	 * buscado
-	 * @return persona Nos devolverá una persona en caso de encontrarla, si no lo puede hacer 
-	 * nos devolverá un elemento nulo.
-	 *
-	 */
-	@Override
-	public Persona buscarId(int id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	/**
-	 * Método para eliminar un contacto de la agenda a partir de un id como parámetro
-	 * @param id Introducimos el identificador del usuario para buscar el contacto a eliminar
-	 */
-	@Override
-	public void delete(int id) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	/**
-	 * Método para devolver la ficha de una persona concreta a partir de su identificador
-	 * @param id Pasamos por parametro el identificador concreto.
-	 * @return devolvemos la ficha del contacto deseado.
-	 */
-	@Override
-	public Persona fichaContacto(int id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	public AgendaDaoImpl(AgendaDao repository) {
+	     this.repository = repository;
+	 }
+	
+	
 }
