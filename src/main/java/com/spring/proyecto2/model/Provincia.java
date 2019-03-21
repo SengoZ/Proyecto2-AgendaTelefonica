3f3af27ede366d24796898fe9bd7cd98995501e6
admin <admin@Profesor>
2019-03-21 17:52:32 +0100

    Nuevos modelos actualizados con entidades

--- src/main/java/com/spring/proyecto2/model/Provincia.java
+++ src/main/java/com/spring/proyecto2/model/Provincia.java
@@ -1,13 +1,9 @@
 package com.spring.proyecto2.model;
 
-import java.io.Serializable;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
+import java.io.Serializable;
+import javax.persistence.*;
+import java.util.List;
 
 /**
  * 
@@ -15,84 +11,60 @@ import javax.persistence.Table;
  *
  */
 
-//Se le da una identidad a la clase y se define el nombre de la tabla en la que se encuentran los objetos de este tipo.
-
 @Entity
-@Table(name = "provincia")
+@NamedQuery(name="Provincia.findAll", query="SELECT p FROM Provincia p")
 public class Provincia implements Serializable {
-
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
-	// Se le da una identidad al iD de la provincia y se indican las columnas
-	// respectivas de cada atributo
-
 	@Id
-	// En principio la expresion de abajo no es necesaria
-	// porque se va a dar una lista de provincias ya generada
-	// por lo que no es necesario crear esa tabla
-	// solo acceder a ella.
-	// @GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer idProvincia;
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int idprovincia;
 
-	@Column(name = "provincia")
 	private String provincia;
 
-	// Se utiliza JoinColumn para relacionar las tablas de Dirección y Provincia a
-	// través del ID de la dirección utilizando OneToOne
+	//bi-directional many-to-one association to Direccion
+	@OneToMany(mappedBy="provincia")
+	private List<Direccion> direccions;
 
-	@JoinColumn(name = "direccion_id", referencedColumnName = "idDireccion")
-	@OneToOne
-	private Provincia provinciaId;
-
-	// Se realizan Getter/Setter de todos los parámetros.
-
-	public Integer getIdProvincia() {
-		return idProvincia;
+	public Provincia() {
 	}
 
-	public void setIdProvincia(Integer idProvincia) {
-		this.idProvincia = idProvincia;
+	public int getIdprovincia() {
+		return this.idprovincia;
+	}
+
+	public void setIdprovincia(int idprovincia) {
+		this.idprovincia = idprovincia;
 	}
 
 	public String getProvincia() {
-		return provincia;
+		return this.provincia;
 	}
 
 	public void setProvincia(String provincia) {
 		this.provincia = provincia;
 	}
 
-	public Provincia getProvinciaId() {
-		return provinciaId;
+	public List<Direccion> getDireccions() {
+		return this.direccions;
 	}
 
-	public void setProvinciaId(Provincia provinciaId) {
-		this.provinciaId = provinciaId;
+	public void setDireccions(List<Direccion> direccions) {
+		this.direccions = direccions;
 	}
 
-	// Se realizan tanto el constructor vacío como el constructor con los atributos
-	// que definen al objeto.
+	public Direccion addDireccion(Direccion direccion) {
+		getDireccions().add(direccion);
+		direccion.setProvincia(this);
 
-	public Provincia() {
-		super();
+		return direccion;
 	}
 
-	public Provincia(Integer idProvincia, String provincia, Provincia provinciaId) {
-		super();
-		this.idProvincia = idProvincia;
-		this.provincia = provincia;
-		this.provinciaId = provinciaId;
-	}
+	public Direccion removeDireccion(Direccion direccion) {
+		getDireccions().remove(direccion);
+		direccion.setProvincia(null);
 
-	// Se realiza el ToString
-
-	@Override
-	public String toString() {
-		return "Provincia [idProvincia=" + idProvincia + ", nombreProvincia=" + provincia + ", provinciaId="
-				+ provinciaId + "]";
+		return direccion;
 	}
 
 }
--- src/main/java/com/spring/proyecto2/model/Telefono.java
+++ src/main/java/com/spring/proyecto2/model/Telefono.java
@@ -1,100 +1,55 @@
 package com.spring.proyecto2.model;
 
 import java.io.Serializable;
+import javax.persistence.*;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
 
 /**
  * 
  * @author Grupo 3: Amador Cáceres, Cesar Marcos, Andrés Gomez y Sheila García
  *
  */
-
-//Se le da una identidad a la clase y se define el nombre de la tabla en la que se encuentran los objetos de este tipo.
-
 @Entity
-@Table(name = "telefono")
+@NamedQuery(name="Telefono.findAll", query="SELECT t FROM Telefono t")
 public class Telefono implements Serializable {
-
-	/**
-	* 
-	*/
 	private static final long serialVersionUID = 1L;
 
-	// Se le da una identidad a la ID del teléfono que será autoincremental.
-	// También se hace referencia a las columnas de la tabla que guardarán cada
-	// atributo del objeto.
-
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer idTelefono;
-	@Column(name = "telefono")
-	private int telefono;
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int idtelefono;
 
-	// Se utiliza JoinColumn para relacionar las tablas de Persona y Telefonos a
-	// través del ID de la persona utilizando OneToOne
+	private String telefono;
 
-	@JoinColumn(name = "persona_id", referencedColumnName = "idpersona")
-	@OneToOne
-	private Persona personaId;
-
-	// Se realizan Getter/Setter de todos los parámetros.
-
-	public Integer getIdTelefono() {
-		return idTelefono;
-	}
-
-	public void setIdTelefono(Integer idTelefono) {
-		this.idTelefono = idTelefono;
-	}
-
-
-	public int getTelefono() {
-		return telefono;
-	}
-
-	public void setTelefono(int telefono) {
-		this.telefono = telefono;
-	}
-
-	public Persona getPersonaId() {
-		return personaId;
-	}
-
-	public void setPersonaId(Persona personaId) {
-		this.personaId = personaId;
-	}
-
-	// Se realizan tanto el constructor vacío como el constructor con los atributos
-	// que definen al objeto.
+	//bi-directional many-to-one association to Persona
+	@ManyToOne
+	@JoinColumn(name="idpersona")
+	private Persona persona;
 
 	public Telefono() {
-		super();
 	}
 
-	public Telefono(Integer idTelefono, int telefono, Persona personaId) {
-		super();
-		this.idTelefono = idTelefono;
+	public int getIdtelefono() {
+		return this.idtelefono;
+	}
+
+	public void setIdtelefono(int idtelefono) {
+		this.idtelefono = idtelefono;
+	}
+
+	public String getTelefono() {
+		return this.telefono;
+	}
+
+	public void setTelefono(String telefono) {
 		this.telefono = telefono;
-		this.personaId = personaId;
 	}
 
-	@Override
-	public String toString() {
-		return "Telefono [idTelefono=" + idTelefono + ", telefono=" + telefono + ", personaId=" + personaId + "]";
+	public Persona getPersona() {
+		return this.persona;
 	}
-	
-	
 
-	// Se realiza el ToString
-
-	
+	public void setPersona(Persona persona) {
+		this.persona = persona;
+	}
 
 }
