a834e951c9ef173be1b0cf2a503622b445492505
admin <admin@Profesor>
2019-03-22 08:43:45 +0100

    formulario.jsp y Junit Alta contacto(con error)

---
+++ src/test/java/com/spring/proyecto2/Pruebas_JPA_AltaContacto.java
@@ -0,0 +1,34 @@
+package com.spring.proyecto2;
+
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.spring.proyecto2.dao.AgendaDao;
+import com.spring.proyecto2.model.Persona;
+
+import junit.framework.TestCase;
+@RunWith(SpringRunner.class)
+public class Pruebas_JPA_AltaContacto extends TestCase {
+
+	
+		@Autowired
+		AgendaDao repository;
+
+		@Test
+		public void testAltaContacto() {
+			repository.deleteAll();
+			Persona persona = new Persona();
+			repository.save(persona);
+			assertEquals(1, repository.count());
+		}
+		
+		
+		
+
+	}
